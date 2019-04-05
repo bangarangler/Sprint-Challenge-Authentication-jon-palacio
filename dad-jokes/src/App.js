c49c7b04db05ae00156d3f3cfc0d0bc9555ed7d8
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-04-05 11:56:01 -0400

    Added in React Client to build out frontend
    installed axios, react-router-dom for now.
    imported router in index. made component folders for Signup, Login,
    Users

---
+++ dad-jokes/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import axios from "axios";
+//import Signup from ''
+//import Login from ''
+//import Users from ''
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>test</p>
+      </div>
+    );
+  }
+}
+
+export default App;
